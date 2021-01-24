e60f27ccb2403ba5359d9003bc3e43d579320b71
Gretchen A <and01487@umn.edu>
2021-01-23 23:31:48 -0600

    append svg element

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,30 @@
+//Make Responsive
+function makeResponsive(){
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;  
+    var margin = {
+        top: 50,
+        bottom: 50,
+        right: 50,
+        left: 50
+    };
+    
+      var height = svgHeight - margin.top - margin.bottom;
+      var width = svgWidth - margin.left - margin.right;
+      // Append SVG element
+  var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append group element
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
 // Load Data from CSV
-d3.csv("assets/data/data.csv").then(function(statedata) {
+d3.cvesv("assets/data/data.csv").then(function(statedata) {
     console.log(statedata);
 // Cast each column as a number using the unary + operator
 statedata.forEach(function(data) {
@@ -9,5 +34,7 @@ statedata.forEach(function(data) {
     data.obesity = +data.obesity;
     data.poverty = +data.poverty;
     data.smokes = +data.smokes;
+    console.log("Age:",data.age);
 });  
 });
+};
