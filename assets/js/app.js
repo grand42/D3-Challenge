15cfdc92ea31301265ae37cd8cbff335c0990528
Gretchen A <and01487@umn.edu>
2021-01-23 23:11:11 -0600

    cast each column

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 // Load Data from CSV
-d3.csv("assets/data/data.csv").then(function(data) {
-    console.log(data);
+d3.csv("assets/data/data.csv").then(function(statedata) {
+    console.log(statedata);
+// Cast each column as a number using the unary + operator
+statedata.forEach(function(data) {
+    data.age = +data.age;
+    data.healthcare = +data.healthcare;
+    data.income = +data.income;
+    data.obesity = +data.obesity;
+    data.poverty = +data.poverty;
+    data.smokes = +data.smokes;
+});  
 });
